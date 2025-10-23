2334d074f5e4b789602baec68ec3e0757fcc9770
Yash Chopra <theyash920@gmail.com>
2025-10-23 12:36:20 +0530

    done two question min and max element and reverse element on array

---
+++ MINMAXELEMENT.cpp
@@ -0,0 +1,48 @@
+// Find the min and max element in the array
+#include <bits/stdc++.h>
+using namespace std;
+
+// User function Template for C++
+class Solution {
+  public:
+    pair<long long, long long> getMinMax(vector<long long int> arr) {
+        int min = arr[0];
+        int max = arr[0];
+        if(arr.size()==1){
+            return{max,min};
+        }
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]<min){
+                min = arr[i];
+            }
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        return {min,max};
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+/*int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<long long int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        pair<long long, long long> pp = ob.getMinMax(arr);
+        cout << pp.first << " " << pp.second << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
---
+++ REVERSEELEMENT.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr){
+        int n= arr.size();
+        int i = 0;
+        int e =n-1;
+        while(i<e){
+            swap(arr[i],arr[e]);
+            i++;
+            e--;
+        }
+    }
+    void printArray(const vector<int> &arr){
+        int n= arr.size();
+        for(int i=0;i<n;i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    
+    }
+};
+// FOR VECTOR ANS ONLY 
+/*
+#include<vector>
+using namespace std;
+vector <int> reverse(vector<int> v){
+    int s =0;
+   int  e = v.size()-1;
+    while(s<=e){
+        swap(v[s],v[e]);
+        s++;
+        e--;
+    }
+    return  v;
+}
+void print(vector <int> v){
+    for(int i=0;i<v.size();i++){
+        cout << v[i] <<" ";
+    }
+    cout<<endl;
+}*/
